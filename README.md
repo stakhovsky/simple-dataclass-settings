658c4ea400dfc77129178a125c518487738e4af6
Egor Stakhovsky <y.stakhovsky@gmail.com>
2022-12-06 21:08:54 +0300

    Initialization

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# simple-dataclass-settings
+# simple_dataclass_settings
+Simple library on top of dataclasses to create shiny-looking configs from environment variables.
+
+## Example usage
+```python3
+import dataclass_env
+
+
+@dataclass_env.settings
+class SomeChildSettings:
+    some_float: float = dataclass_env.field.float(
+        var='SOME_FLOAT',
+    )
+
+
+@dataclass_env.settings
+class Settings:
+    some_child: SomeChildSettings
+    some_string: str = dataclass_env.field.str(
+        var='SOME_STRING',
+        default='test',
+    )
+    some_number: int = dataclass_env.field.int(
+        var='SOME_NUMBER',
+        default=69,
+    )
+    some_flag: bool = dataclass_env.field.bool(
+        var='SOME_FLAG',
+        default=False,
+    )
+    some_list: bool = dataclass_env.field.str_list(
+        var='SOME_LIST',
+        default=['test', 'vars'],
+    )
+
+
+settings = dataclass_env.populate(Settings)
+```
+
+## Interface
+ - `settings` - decorator that converts a class to the settings class (just a regular frozen (and slotted) dataclass)
+ - `field` - class that creates a special setting field object (that holds metadata about environment variables and its processing)
+ - `populate` - function that creates an instance of passed settings class
+ - `show` - function that shows a list of environment variables used by the passed class
---
+++ simple_dataclass_settings.py
@@ -0,0 +1,333 @@
+import copy
+import dataclasses
+import functools
+import os
+import sys
+import typing
+import warnings
+
+
+__version__ = '0.0.1'
+
+
+_DC = typing.TypeVar('_DC')
+
+
+_MISSING = object()
+
+
+def _bool(
+    value: typing.Union[str, bool, int],
+) -> bool:
+    if isinstance(value, bool):
+        return value
+    if isinstance(value, int):
+        return bool(value)
+    return value.lower().strip() in ('1', 't', 'y', 'true', 'yes')
+
+
+def _int(
+    value: typing.Union[str, int],
+    min_value: int = _MISSING,
+    max_value: int = _MISSING,
+    default: int = _MISSING,
+) -> int:
+    result = value
+    if isinstance(result, str):
+        try:
+            result = int(result.lower().strip())
+        except TypeError:
+            if default is _MISSING:
+                raise
+            result = default
+
+    if (min_value is not _MISSING) and (result < min_value):
+        result = min_value
+    if (max_value is not _MISSING) and (result > max_value):
+        result = max_value
+
+    return result
+
+
+def _float(
+    value: typing.Union[str, float],
+    min_value: float = _MISSING,
+    max_value: float = _MISSING,
+    default: float = _MISSING,
+) -> float:
+    result = value
+    if isinstance(result, str):
+        try:
+            result = result.lower().strip()
+            if ('.' not in result) and (',' in result):
+                result = result.replace(',', '.', 1)
+            result = float(result)
+        except TypeError:
+            if default is _MISSING:
+                raise
+            result = default
+
+    if (min_value is not _MISSING) and (result < min_value):
+        result = min_value
+    if (max_value is not _MISSING) and (result > max_value):
+        result = max_value
+
+    return result
+
+
+class Field:
+    __slots__ = (
+        '_var',
+        '_cast',
+        '_default',
+        '_default_factory',
+    )
+
+    def __init__(
+        self,
+        var: typing.Optional[str] = None,
+        cast: typing.Callable[[str], typing.Any] = _MISSING,
+        default: typing.Any = _MISSING,
+        default_factory: typing.Any = _MISSING,
+    ) -> None:
+        self._var = var
+        self._cast = cast
+        self._default = default
+        self._default_factory = default_factory
+
+    @property
+    def var(self):
+        return self._var
+
+    @property
+    def cast(self):
+        return self._cast
+
+    @property
+    def has_default(self):
+        return (self._default is not _MISSING) or (self._default_factory is not _MISSING)
+
+    def get_default(self):
+        if self._default is not _MISSING:
+            return copy.deepcopy(self._default)
+        elif self._default_factory is not _MISSING:
+            return copy.deepcopy(self._default_factory())
+        raise EnvironmentError(f"{self.var} is not set")
+
+    @classmethod
+    def str_(
+        cls,
+        var: str,
+        default: str = _MISSING,
+    ) -> 'Field':
+        return cls(
+            var=var,
+            cast=str,
+            default=default,
+        )
+
+    @classmethod
+    def int_(
+        cls,
+        var: str,
+        min_value: int = _MISSING,
+        max_value: int = _MISSING,
+        default: int = _MISSING,
+    ) -> 'Field':
+        return cls(
+            var=var,
+            cast=functools.partial(
+                _int,
+                min_value=min_value,
+                max_value=max_value,
+                default=default,
+            ),
+            default=default,
+        )
+
+    @classmethod
+    def float_(
+        cls,
+        var: str,
+        min_value: float = _MISSING,
+        max_value: float = _MISSING,
+        default: float = _MISSING,
+    ) -> 'Field':
+        return cls(
+            var=var,
+            cast=functools.partial(
+                _float,
+                min_value=min_value,
+                max_value=max_value,
+                default=default,
+            ),
+            default=default,
+        )
+
+    number = float_
+
+    @classmethod
+    def bool_(
+        cls,
+        var: str,
+        default: bool = _MISSING,
+    ) -> 'Field':
+        return cls(
+            var=var,
+            cast=_bool,
+            default=default,
+        )
+
+    @classmethod
+    def list_(
+        cls,
+        var: str,
+        sub_cast: typing.Callable[[typing.Any], typing.Any] = lambda x: str(x),
+        default: typing.Sequence[typing.Any] = _MISSING,
+    ) -> 'Field':
+        return cls(
+            var=var,
+            cast=lambda val: [sub_cast(x) for x in val.strip().split(',') if x.strip()],
+            default=default,
+        )
+
+    @classmethod
+    def str_list(
+        cls,
+        var: str,
+        default: typing.Sequence[str] = _MISSING,
+    ) -> 'Field':
+        return cls.list_(
+            var=var,
+            sub_cast=str,
+            default=default,
+        )
+
+    @classmethod
+    def int_list(
+        cls,
+        var: str,
+        min_value: int = _MISSING,
+        max_value: int = _MISSING,
+        value_default: int = _MISSING,
+        default: typing.Sequence[int] = _MISSING,
+    ) -> 'Field':
+        return cls.list_(
+            var=var,
+            sub_cast=functools.partial(
+                _int,
+                min_value=min_value,
+                max_value=max_value,
+                default=value_default,
+            ),
+            default=default,
+        )
+
+    @classmethod
+    def float_list(
+        cls,
+        var: str,
+        min_value: float = _MISSING,
+        max_value: float = _MISSING,
+        value_default: float = _MISSING,
+        default: typing.Sequence[float] = _MISSING,
+    ) -> 'Field':
+        return cls.list_(
+            var=var,
+            sub_cast=functools.partial(
+                _int,
+                min_value=min_value,
+                max_value=max_value,
+                default=value_default,
+            ),
+            default=default,
+        )
+
+    number_list = float_list
+
+    str = str_
+    int = int_
+    float = float_
+    list = list_
+
+
+field = Field
+
+
+def populate(
+    cls: typing.Type[_DC],
+) -> _DC:
+    """
+    Creates an instance of passed settings class.
+    :param cls: Settings class
+    :return: Instance of the passed class
+    """
+    params = {}
+    for fld in dataclasses.fields(cls):
+        if dataclasses.is_dataclass(fld.type):
+            if isinstance(fld.default, Field):
+                if fld.default.cast is not _MISSING:
+                    warnings.warn(
+                        "\"cast\" can not be used on sub-settings. "
+                        f"Please check the \"{fld.type.__name__}\" at the \"{cls.__name__}\"."
+                    )
+                if fld.default.has_default:
+                    warnings.warn(
+                        "Neither \"default\" nor \"default_factory\" "
+                        "can not be used on sub-settings. "
+                        f"Please check the \"{fld.type.__name__}\" at the \"{cls.__name__}\"."
+                    )
+
+            params[fld.name] = populate(
+                cls=fld.type,
+            )
+            continue
+
+        if not isinstance(fld.default, Field):
+            continue
+
+        if fld.default.cast is _MISSING:
+            raise EnvironmentError(f"\"{cls.__name__}\" has no cast for \"{fld.name}\".")
+
+        value = os.environ.get(fld.default.var, _MISSING)
+        if value is not _MISSING:
+            value = fld.default.cast(value)
+        else:
+            value = fld.default.get_default()
+
+        params[fld.name] = value
+    return cls(**params)
+
+
+def show(
+    cls: typing.Type[_DC],
+) -> None:
+    """
+    Shows a list of environment variables used by the passed class.
+    :param cls: Settings class
+    :return:
+    """
+    for fld in dataclasses.fields(cls):
+        if dataclasses.is_dataclass(fld.type):
+            show(fld.type)
+            continue
+
+        if not isinstance(fld.default, Field):
+            continue
+
+        line_ = f"{fld.default.var}="
+        if fld.default.has_default:
+            line_ += str(fld.default.get_default())
+
+        print(line_)
+
+
+if sys.version_info[:2] < (3, 10):
+    settings = dataclasses.dataclass(
+        frozen=True,
+    )
+else:
+    settings = dataclasses.dataclass(
+        slots=True,
+        frozen=True,
+    )
